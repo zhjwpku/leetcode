690fef191e128a97f453d77d3b935d20f8d41b1c
Zhao Junwang <zhjwpku@gmail.com>
2015-09-02 19:32:48 +0800

    simplify path

---
+++ 71_simplify_path.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string simplifyPath(string path) {
+	vector<string> dirs;
+	for (auto i = path.begin(); i != path.end();) {
+		i++;
+
+		auto j = find(i, path.end(), '/');
+		auto dir = string(i, j);
+		if (!dir.empty() && dir != ".") {
+			if (dir == "..") {
+				if (!dirs.empty())
+					dirs.pop_back();
+			} else
+				dirs.push_back(dir);
+		}
+
+		i = j;
+	}
+
+	stringstream out;
+	if (dirs.empty()) {
+		out << "/";
+	} else {
+		for (auto dir : dirs)
+			out << "/" << dir;
+	}
+
+	return out.str();
+}
