28a637e4495336e3c7b62eca9e87af609c055249
Zhao Junwang <zhjwpku@gmail.com>
2015-09-02 14:56:56 +0800

    3sum using 2sum

--- 15_3sum.cpp
+++ 15_3sum.cpp
@@ -26,3 +26,38 @@ vector<vector<int>> threeSum(vector<int> &num) {
 	result.erase(unique(result.begin(), result.end()), result.end());
 	return result;
 }
+
+/* using 2 sum */
+vector<vector<int> > threeSum(vector<int> &nums) {
+	vector<vector<int> > results;
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < nums.size(); i++) {
+		twoSum(i, nums, results, 0 - nums[i]);
+
+		while (i < nums.size()-1 && nums[i] == nums[i+1])
+			i++;
+	}
+
+	return results;
+}
+
+void twoSum(int index, vector<int> &nums, vector<vector<int> > &results, int sum) {
+	unordered_map<int, int> record;
+
+	for (int i = index + 1; i < nums.size(); i++) {
+		if (record.find(nums[i]) == record.end())
+			record[sum-nums[i]] = nums[i];
+		else {
+			vector<int> vec;
+			vec.push_back(nums[index]);
+			vec.push_back(record[nums[i]]);
+			vec.push_back(nums[i]);
+
+			results.push_back(vec);
+
+			while (i < nums.size() - 1 && nums[i] == nums[i+1])
+				i++;
+		}
+	}
+}
