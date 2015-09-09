a2d3ff5a18a3abaf07318ddb0e95f7b126afd2a3
Zhao Junwang <zhjwpku@gmail.com>
2015-09-09 14:18:07 +0800

    invert binary tree

---
+++ 226_invert_binary_tree.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+TreeNode *invertTree(TreeNode* root) {
+	if (root == NULL) return root;
+
+	TreeNode *tmp = root->left;
+	root->left = root->right;
+	root->right = tmp;
+
+	invertTree(root->left);
+	invertTree(root->right);
+
+	return root;
+}
