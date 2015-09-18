65924421b7d217db40a5f6dfa7f7f6a40d3a2fac
Zhao Junwang <zhjwpku@gmail.com>
2015-09-18 21:42:24 +0800

    first missing positive

---
+++ 41_first_missing_positive.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int firstMissingPostive(vector<int>& nums) {
+	if (nums.size() == 0) return 1;
+	for (int i = 0; i < nums.size(); i++) {
+		while (nums[i] != i+1) {
+			if (nums[i] <= 0 || nums[i] > nums.size() || nums[i] == nums[nums[i] - 1])
+				break;
+
+			swap(nums[i], nums[nums[i] - 1]);
+		}
+	}
+
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] != i+1)
+			return i+1;
+	}
+
+	return nums.size() + 1;
+}
